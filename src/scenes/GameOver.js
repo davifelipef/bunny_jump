677b6bbe22102d5b03feefa7a082a7dc7ce314f4
davifelipef <feliperedfield@gmail.com>
2023-07-07 23:40:44 -0300

    Update GameOver.js

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,24 +1,31 @@
 import Phaser from '../lib/phaser.js'
 
-export default class GameOver extends Phaser.Scene
-{
-    constructor()
-    {
+export default class GameOver extends Phaser.Scene {
+    constructor() {
         super('game-over')
     }
 
-    create()
-    {
+    create() {
         const width = this.scale.width
         const height = this.scale.height
 
         this.add.text(width * 0.5, height * 0.5, 'Game Over', {
-        fontSize: 48
+            fontSize: 48
         })
         .setOrigin(0.5)
 
-        this.input.keyboard.once('keydown-SPACE', () => {
+        const restartGame = () => {
             this.scene.start('game')
+        }
+
+        // Handle touch input
+        this.input.on('pointerdown', () => {
+            restartGame()
+        })
+
+        // Handle space key for PC
+        this.input.keyboard.once('keydown-SPACE', () => {
+            restartGame()
         })
     }
 }
