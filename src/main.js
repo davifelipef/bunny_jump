89b0647e1aab52a9a74078ebed277285d494de6d
davifelipef <feliperedfield@gmail.com>
2023-07-07 23:31:26 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import GameOver from './scenes/GameOver.js'
 
 console.dir(Phaser)
 
-// Calculate the width based on the device's screen size
-const width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: width,
-    height: 640,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 480,
+        height: 640
+    },
     scene: [Game, GameOver],
     physics: {
         default: 'arcade',
@@ -21,4 +22,5 @@ export default new Phaser.Game({
             debug: false
         }
     }
-})
+});
+
